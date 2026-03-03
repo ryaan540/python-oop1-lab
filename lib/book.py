08dd6ea26fc4414aab35e87a1a78b536e6584024
ryaan540! <rkinyua841@gmail.com>
2026-03-03 14:50:03 +0300

    Add Book and Coffee classes with validation

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,29 @@
 #!/usr/bin/env python3
 
 class Book:
+    """
+    Represents a book in the bookstore.
+    """
+
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count  # this will trigger the property setter
+
+    # Property getter
+    @property
+    def page_count(self):
+        return self._page_count
+
+    # Property setter with validation
+    @page_count.setter
+    def page_count(self, value):
+        if isinstance(value, int):
+            self._page_count = value
+        else:
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
     pass
     
         
--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python3
 
 class Coffee:
+    
+    """
+    Represents a coffee sold in the bookstore.
+    """
+
+    def __init__(self, size, price):
+        self.size = size  # validated by property
+        self.price = price
+
+    # Size property getter
+    @property
+    def size(self):
+        return self._size
+
+    # Size property setter with validation
+    @size.setter
+    def size(self, value):
+        if value in ["Small", "Medium", "Large"]:
+            self._size = value
+        else:
+            print("size must be Small, Medium, or Large")
+
+    def tip(self):
+        print("This coffee is great, here’s a tip!")
+        self.price += 1
     pass
